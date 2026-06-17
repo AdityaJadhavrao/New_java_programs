a05d7535f384c5eb54d3c22c78abb29939a4e395
Prithviraj Jadhavrao <adijadhavrao@gmail.com>
2026-06-17 12:22:10 +0530

    missing number from Array

--- for_loop/missingNumber.java
+++ for_loop/missingNumber.java
@@ -2,20 +2,18 @@ package for_loop;
 
 public class missingNumber {
     
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,3,4,6};
+        int n = arr.length+1; //6
+        int sum = (n*(n+1))/2; //21
 
-    public static void main(String args[])
-    {
-        int a[] = {1,2,3,4,6};
-
-        int n = 6;
-        int expectedSum = n*(n+1)/2;
-        int actualSum=0;
-
-        for(int num : a)
+        for(int i=0;i<arr.length;i++)
         {
-            actualSum+=num;
+            sum = sum-arr[i];
         }
 
-        System.out.println("Missing number is :" + (expectedSum-actualSum));
+        System.out.println("Missing number is "+sum);
     }
 }
+
